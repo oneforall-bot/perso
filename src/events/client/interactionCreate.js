38f543bd7eb74b0fb6e9f922eeecf7f082c76751
TakeFy <79745753+takefy-dev@users.noreply.github.com>
2021-12-01 13:39:45 +0100

    slasg

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -20,7 +20,8 @@ module.exports = async (ftSecurity, interaction) => {
         memberData.permissionManager = new ftSecurity.Permission(ftSecurity, interaction.member.guild.id, interaction.member.id, memberData, guildData);
 
         const target = interaction.isContextMenu() ? (interaction.targetType === "USER" ? interaction.targetId : (interaction.targetType === "MESSAGE" ? (await interaction.channel.messages.fetch(interaction.targetId) || null) : null)) : null;
-
+        if (slash.ownersOnly && !ftSecurity.config.owners.includes(interaction.user.id))
+            return interaction.reply({content: ftSecurity.langManager().get(guildData.lang).notOwner("/", slash.data)});
         slash.run(ftSecurity, interaction, memberData, guildData, target);
         console.log(`Slash command: ${slash.data.name} has been executed by ${interaction.user.username}#${interaction.user.discriminator} in ${interaction.guild.name}`);
     }
