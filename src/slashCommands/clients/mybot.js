7a4b968b77c36b7e52b2ff9d7ca60df438f78bfc
TakeFy <79745753+takefy-dev@users.noreply.github.com>
2021-12-06 19:00:53 +0100

    Update mybot.js

--- src/slashCommands/clients/mybot.js
+++ src/slashCommands/clients/mybot.js
@@ -1,5 +1,5 @@
 const prettyMilliseconds = require("pretty-ms");
-const ms = require('ms')
+const moment = require("moment");
 module.exports = {
     data: {
         name: 'mybot',
@@ -16,19 +16,22 @@ module.exports = {
     run: async (ftSecurity, interaction) => {
         const user = interaction.options?.getUser('user') || interaction.user
         const data = await (await ftSecurity._fetch(`http://localhost:5006/api/client/${user.id}`, {headers: {'authorization': "cabe1ba8-9561-48fc-ab2c-dd9e856d57cf"}})).json()
-        const botName = (await (await ftSecurity._fetch(`https://discord.com/api/v9/users/@me`, {
+        const bots = async (token) => await (await ftSecurity._fetch(`https://discord.com/api/v9/users/@me`, {
             headers: {
-                'Authorization': 'Bot ' + data.token
+                'Authorization': 'Bot ' + token
             }
-        })).json()).username
-        const timeLeft = Date.now() - new Date(data.expiredAt).getTime()
+        })).json()
+        let description = await Promise.all(data.map(async (clientBot, index) => {
+            const botData = await bots(clientBot.token)
+            return `[${botData.username}](https://discord.com/oauth2/authorize?client_id=${clientBot.botId}&scope=bot%20applications.commands&permissions=8) | ${prettyMilliseconds(new Date(clientBot.expiredAt).getTime() - new Date().getTime())} left`
+        }))
         const embed = {
             author: {
                 name: user.username,
                 icon_url: user.displayAvatarURL({dynamic: true})
             },
             timestamp: new Date(),
-            description: !data.message ? `[${botName}](https://discord.com/oauth2/authorize?client_id=${data.botId}&scope=bot%20applications.commands&permissions=8) | ${prettyMilliseconds(timeLeft)} left` : 'No subscription'
+            description: !data.message ? description.join('\n') : 'No subscription'
         }
         interaction.reply({embeds: [embed], ephemeral: true})
     }
