9f9163ccc7f4549aa0a8d523da254f3972ad40c3
TakeFy <79745753+takefy-dev@users.noreply.github.com>
2021-12-22 21:58:51 +0100

    Update index.js

--- src/managers/guilds/index.js
+++ src/managers/guilds/index.js
@@ -14,7 +14,7 @@ module.exports = function (database, modelName, config) {
                 isValue: true,
                 type: DataTypes.STRING(25),
                 allowNull: false,
-                default: config.prefix
+                default: '!'
             },
             {
                 name: "lang",
