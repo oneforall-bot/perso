5d0137849e0fc5c5a083694b1bbaaf35d1772ce9
TakeFy <79745753+takefy-dev@users.noreply.github.com>
2021-12-25 21:08:51 +0100

    Update setactivity.js

--- src/commands/buyer/setactivity.js
+++ src/commands/buyer/setactivity.js
@@ -18,40 +18,23 @@ module.exports = {
     * @param {[]} args
     */
     run: async (oneforall, message, guildData, memberData, args) => {
-        
+        const types=  ['steaming', 'playing', 'watching']
+        if(!args[0] || !types.includes(args[0].toLowerCase())) return message.channel.send('Missing type')
         const activityName = args.slice(1).join(" ");
-        if (args[0].toLowerCase() === "streaming") {
-            oneforall.user.setPresence({
-                activity: {
-                    name: activityName,
-                    type: 'STREAMING',
-                    url: "https://www.twitch.tv/discord"
-                }
-            })
-                .then(p => message.channel.send(`${message.author}, Vous avez définis le statut de votre bot en \`${activityName.replace("streaming", " ")}\`.`))
-                .catch(e => {
-                    return message.channel.send(`${message.author}, Une erreur a été rencontré. \n **Plus d'informations:** \`🔻\` \`\`\`${e}\`\`\``);
-                });
 
-
-        } else if (args[0].toLowerCase() === "playing") {
-            oneforall.user.setPresence({activity: {name: activityName, type: 'PLAYING'}})
-                .then(p => message.channel.send(`${message.author}, Vous avez définis le statut de votre bot en \`${activityName.replace("playing", " ")}\`.`))
-                .catch(e => {
-                    return message.channel.send(`${message.author}, Une erreur a été rencontré. \n **Plus d'informations:** \`🔻\` \`\`\`${e}\`\`\``);
-                });
-        } else if (args[0] === "watching") {
-            oneforall.user.setPresence({activity: {name: activityName, type: 'WATCHING'}})
-                .then(p => message.channel.send(`${message.author}, Vous avez définis le statut de votre bot en \`${activityName.replace("watching", " ")}\`.`))
-                .catch(e => {
-                    return message.channel.send(`${message.author}, Une erreur a été rencontré. \n **Plus d'informations:** \`🔻\` \`\`\`${e}\`\`\``);
-                });
-        } else if (!args[0]) {
-            message.channel.send(`${message.author}, Vous avez fournie aucune valeur pour l'activité`);
-
-        } else if (args[0] && !args[1]) {
-            message.channel.send(`${message.author}, Vous avez fournie aucune valeur pour le nom de l'activié`);
-
-        }
+        const activities = [{
+            name: activityName,
+            type: args[0].toLowerCase(),
+            url: 'https://www.twitch.tv/discord'
+        }]
+        oneforall._fetch(`http://localhost:5006/api/client/${oneforall.config.client}/${oneforall.user.id}`, {
+            method: 'patch',
+            body: JSON.stringify({ activity: activities }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'cabe1ba8-9561-48fc-ab2c-dd9e856d57cf'
+            }
+        })
+        oneforall.user.setPresence({status: 'online', activities})
     }
 }
